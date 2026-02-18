1c9b29c3ea83c4058a3d6f4e4490945159401608
aarav-arch <bostonaarav2@gmail.com>
2026-02-18 13:31:04 -0500

    Added a introduction to myself in the lab_1a.py file.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Aarav Garg" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello, my name is Aarav Garg. I am a freshman in Newton, Massachusetts. I am an avid skier, and like to 3D print and play GTA in my free time. I have a mild MTN Dew addiction")
 
 if __name__ == "__main__":
     main()
